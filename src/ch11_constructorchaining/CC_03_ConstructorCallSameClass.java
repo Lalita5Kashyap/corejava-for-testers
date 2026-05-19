7a6c8d781a7d1cd0cc37b9e21aa4b47f7d85e4d9
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-19 10:19:30 +0530

    push changes

--- src/ch11_constructorchaining/CC_03_ConstructorCallSameClass.java
+++ src/ch11_constructorchaining/CC_03_ConstructorCallSameClass.java
@@ -1,9 +1,21 @@
 package ch11_constructorchaining;
 
 public class CC_03_ConstructorCallSameClass {
-
-	public static void main(String[] args) {
-		
+	// no-arg constructor
+	CC_03_ConstructorCallSameClass(String name) {
+		System.out.println("Employee info");
+		System.out.println("name :" + name);
 	}
 
+	// Argument construct
+	CC_03_ConstructorCallSameClass(String name, int id) {
+		this(name);
+		System.out.println("id :" + id);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Main starts");
+		new CC_03_ConstructorCallSameClass("Kirthi", 89);
+		new CC_03_ConstructorCallSameClass("Lokesh", 01);
+	}
 }
