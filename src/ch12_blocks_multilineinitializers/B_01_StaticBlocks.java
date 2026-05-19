7987fa612f85874306f927edf78af9277840ddc3
Lalita5Kashyap <lalitakashyaplk5@gmail.com>
2026-05-19 11:56:06 +0530

    push changes

--- src/ch12_blocks_multilineinitializers/B_01_StaticBlocks.java
+++ src/ch12_blocks_multilineinitializers/B_01_StaticBlocks.java
@@ -1,9 +1,12 @@
 package ch12_blocks_multilineinitializers;
 
 public class B_01_StaticBlocks {
-
-	public static void main(String[] args) {
-		
+	// static block
+	static {
+		System.out.println("Static block");
 	}
 
+	public static void main(String[] args) {
+		System.out.println("Main method starts");
+	}
 }
